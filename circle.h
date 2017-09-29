d6cb6512092fe024b6cf7eb6a61473652ca19555
DavisRomn <davisabr@gmail.com>
2017-09-29 11:25:44 -0600

    Added circle.h

---
+++ circle.h
@@ -0,0 +1,21 @@
+#ifndef CIRCLE_M
+#define CIRCLE_M
+
+class Circle : public Shape
+{
+public:
+    int radius;
+public:
+    Square(int x, int y, int radii, char* circleName);
+    double area() const;
+    double perimeter() const;
+    Point getOrigin() const;
+    void setOrigin(Point a);
+    void setRadius(int a);
+    int getRadius() const;
+    void setCircleName(char* name);
+    char* getCircleName() const;
+    void display() const;
+};
+
+#endif
